485c0b6649f0425d225a9a5bf6ebfa583e252925
Alex Zimmer <alexzimmer.zimmer1@gmail.com>
2021-10-22 11:31:26 -0300

    SDL Hello_World and draw Image

---
+++ Games/Beginning/HelloWorldSDL/Event_Driven_Programming.cpp
@@ -0,0 +1,19 @@
+#include <SDL.h>
+#include <stdio.h>
+
+bool quit = false;
+
+SDL_Event e;
+/*
+
+while (!quit)
+{
+	while (SDL_PollEvent(&e) != 0)
+	{
+		if (e.type == SDL_QUIT)
+		{
+			quit = true;
+		}
+	}
+}
+*/
